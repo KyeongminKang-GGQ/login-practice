e42888f0d7ab4eeb72ddeda9874ad24c6fb9c651
mia <kyeongmin.kang@ggq.gg>
2022-06-09 14:40:51 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set("views", "./src/views");
+
+// 뷰 엔진 해석
+app.set("view engine", "ejs");
+
+// Rounter와 Controller 분리
+// app.get("/", (req, res) => {
+//     // res.send("here is root");
+//     res.render("home/index");
+// });
+
+// app.get("/login", (req, res) => {
+//     res.render("home/login");
+// });
+
+// 미들웨어 등록
+app.use("/", home);
+
+// 서버 실행
+module.exports = app;
+
+// app.listen(PORT, () => {
+//     console.log("server start");
+// });
