3f3624f6ae0f556dfdbf2c5825b9211f5e0d1433
mia <kyeongmin.kang@ggq.gg>
2022-06-09 15:12:19 +0900

    로그인 기능 생성

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 
@@ -26,6 +27,11 @@ app.set("view engine", "ejs");
 
 // 정적 경로 추가
 app.use(express.static(`${__dirname}/src/public`));
+
+// bodyParser 미들웨어 등록
+app.use(bodyParser.json());
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함 될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // 미들웨어 등록
 app.use("/", home);
