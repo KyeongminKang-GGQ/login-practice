dcc527b6bb6dfb8cc43bc510e8bb6ff60538a26b
mia <kyeongmin.kang@ggq.gg>
2022-06-09 15:18:21 +0900

    로그인 데이터 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,11 @@
 "use strict";
 
+// For test
+const users = {
+    id: ["test1", "test2", "test3"],
+    password: ["123", "1234", "12345"],
+}
+
 const output = {
     home: (req, res) => {
         // res.send("here is root");
@@ -13,6 +19,23 @@ const output = {
 const process = {
     login: (req, res) => {
         console.log(req.body);
+
+        const id = req.body.id,
+        password = req.body.password;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.password[idx] === password) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "Fail to login",
+        })
     }
 }
 
