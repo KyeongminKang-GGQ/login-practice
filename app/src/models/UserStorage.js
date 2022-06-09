f5048c008e31ca38d69dc9f9eee8511405629027
mia <kyeongmin.kang@ggq.gg>
2022-06-09 15:54:53 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -11,14 +11,23 @@ class UserStorage {
     static getUsers(...fields) {
         const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
-            console.log(`reduce : ${newUsers}, ${field}`);
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
             }
             return newUsers;
         }, {});
-        console.log(`userList: ${newUsers}`);
         return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userInfo = Object.keys(users).reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
     }
 }
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
@@ -14,26 +14,10 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const user = new User(req.body);
 
-        const id = req.body.id,
-            password = req.body.password;
-
-        
-        const users = UserStorage.getUsers("id", "password");
-
-        const response = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.password[idx] === password) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "Fail to login";
-
+        const response = user.login();
+    
         return res.json(response);
     }
 }
